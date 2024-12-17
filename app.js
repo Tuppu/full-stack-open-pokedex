3a43e4d487c21462fa0047aa7505104abf58569f
Tuomas Liikala <tuomas.liikala@kajapro.fi>
2024-12-17 09:59:42 +0200

    11.19 Periodic health check

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const PORT = process.env.PORT || 5000
 app.use(expressStatic('dist'))
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  //if (true) throw('error...  ')
   res.send('ok')
 })
 
